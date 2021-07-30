fcc6730c7265c18553a2957cccb8e7c499143272
Mahdi Zareei <mahdizareei.hmd@gmail.com>
2021-07-30 21:35:12 +0430

    .

--- app/src/main/java/com/jetpackcompose/app/MainActivity.kt
+++ app/src/main/java/com/jetpackcompose/app/MainActivity.kt
@@ -1,25 +1,36 @@
 package com.jetpackcompose.app
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jetpackcompose.app.ui.theme.Black
 import com.jetpackcompose.app.ui.theme.ComposeAppTheme
+import com.jetpackcompose.app.ui.theme.Purple700
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeAppTheme {
-                MessageCard(
-                    Message(
-                        author = "Mahdi Zareei",
-                        body = "is android developer"
-                    )
-                )
+                Column() {
+                    MessageCard(msg = Message("Mahdi", "Hi"))
+                    MessageCard(msg = Message("Mahdi", "Hi"))
+                    MessageCard(msg = Message("Mahdi", "Hi"))
+                    MessageCard(msg = Message("Mahdi", "Hi"))
+                    MessageCard(msg = Message("Mahdi", "Hi"))
+                }
             }
         }
     }
@@ -29,14 +40,35 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Column() {
-        Text(text = msg.author)
-        Text(text = msg.body)
+    Row(modifier = Modifier.padding(all = 5.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "user image profile",
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)
+                .border(1.dp, Purple700, CircleShape)
+        )
+
+        Spacer(Modifier.width(8.dp))
+
+        Column() {
+            Text(text = msg.author, color = Black)
+            Spacer(modifier = Modifier.height(5.dp))
+            Text(text = msg.body)
+        }
     }
 }
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(msg = Message("Hi", "Dear"))
+    ComposeAppTheme {
+        MessageCard(msg = Message("Mahdi", "hi im android developer"))
+    }
 }
