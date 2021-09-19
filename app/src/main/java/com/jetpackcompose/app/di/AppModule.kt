87fec2769d06774b602081229300bf5fcdb01f3f
Mahdi Zareei <mahdizareei.hmd@gmail.com>
2021-09-20 00:47:58 +0430

    change provideToken position ti NetworkModule.kt

--- app/src/main/java/com/jetpackcompose/app/di/AppModule.kt
+++ app/src/main/java/com/jetpackcompose/app/di/AppModule.kt
@@ -1,11 +1,8 @@
 package com.jetpackcompose.app.di
 
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Named
-import javax.inject.Singleton
 
 /**
  *  you should give the scope to @InstallIn() to inject something based on this document
@@ -13,15 +10,4 @@ import javax.inject.Singleton
  */
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule {
-
-    //the @Named help us to inject a specific string into a class if we have multiple string
-    //we can get token from server and store it with sharedPreferences and get it in TokenManager class
-    //and remove this provider
-    @Singleton
-    @Provides
-    @Named("token")
-    fun provideToken(): String {
-        return "Token 9c8b06d329136da358c2d00e76946b0111ce2c48"
-    }
-}
+object AppModule {}
--- domain/src/main/java/com/jetpackcompose/domain/di/NetworkModule.kt
+++ domain/src/main/java/com/jetpackcompose/domain/di/NetworkModule.kt
@@ -45,6 +45,16 @@ object NetworkModule {
         return TokenManagerImpl(token)
     }
 
+    //the @Named help us to inject a specific string into a class if we have multiple string
+    //we can get token from server and store it with sharedPreferences and get it in TokenManager class
+    //and remove this provider
+    @Singleton
+    @Provides
+    @Named("token")
+    fun provideToken(): String {
+        return "Token 9c8b06d329136da358c2d00e76946b0111ce2c48"
+    }
+
     private fun okHttpClient(interceptor: Interceptor): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(interceptor)
