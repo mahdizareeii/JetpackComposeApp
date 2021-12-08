ec47a1e9668bf81326c4f2affe02b39e75b9329f
Mahdizareeii <mahdizareei.hmd@gmail.com>
2021-12-08 23:29:00 +0330

    clean up code of the project, save state of lazy row in home screen

--- features/homepage/src/main/java/com/jetpackcompose/homepage/presentation/HomeScreenViewModel.kt
+++ features/homepage/src/main/java/com/jetpackcompose/homepage/presentation/HomeScreenViewModel.kt
@@ -34,7 +34,6 @@ class HomeScreenViewModel @Inject constructor(
     private val _selectedCategory: MutableState<FoodCategory> = mutableStateOf(FoodCategory.UN_KNOW)
     val selectedCategory: State<FoodCategory> get() = _selectedCategory
 
-
     //save scroll position state of the lazy row in home screen
     var lazyRowStatePosition = 0
     var lazyRowScrollOffsetPosition = 0
