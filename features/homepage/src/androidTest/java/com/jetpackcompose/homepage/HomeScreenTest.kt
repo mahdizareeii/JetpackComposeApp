11cb263ae9fad61d213e97f48f40e46f3465942b
M.Zareei <m.zareei@jabama.com>
2022-01-15 17:07:24 +0330

    write first instrument test for jetpack compose

--- features/homepage/src/androidTest/java/com/jetpackcompose/homepage/HomeScreenTest.kt
+++ features/homepage/src/androidTest/java/com/jetpackcompose/homepage/HomeScreenTest.kt
@@ -1,4 +1,4 @@
-package com.jetpackcompose.app
+package com.jetpackcompose.homepage
 
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
