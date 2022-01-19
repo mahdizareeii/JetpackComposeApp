9776dd476140c194ffe2a2209503d17efb40e08c
M.Zareei <m.zareei@jabama.com>
2022-01-19 12:06:44 +0330

    handle server error in detail screen

--- features/detailpage/src/main/java/com/jetpackcompose/detailpage/presentation/DetailScreen.kt
+++ features/detailpage/src/main/java/com/jetpackcompose/detailpage/presentation/DetailScreen.kt
@@ -1,28 +1,23 @@
 package com.jetpackcompose.detailpage.presentation
 
 import android.os.Bundle
-import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.compose.rememberImagePainter
-import coil.size.Scale
 import com.jetpackcompose.detailpage.presentation.components.DetailContent
 import com.jetpackcompose.resources.components.CircularProgress
-import com.jetpackcompose.resources.components.SquareView
+import com.jetpackcompose.resources.components.ScreenError
 
 @ExperimentalFoundationApi
 @ExperimentalAnimationApi
@@ -31,16 +26,28 @@ fun DetailScreen(
     viewModel: DetailScreenViewModel = hiltViewModel(),
     argument: Bundle?
 ) {
-    var expanded by remember {
+    var loadingVisibility by remember {
         mutableStateOf(false)
     }
 
-    viewModel.loading.value.let {
-        expanded = !it
+    var errorVisibility by remember {
+        mutableStateOf(false)
     }
 
-    viewModel.error.value?.let {
+    var contentVisibility by remember {
+        mutableStateOf(false)
+    }
 
+    LaunchedEffect(viewModel.detail.value) {
+        contentVisibility = viewModel.detail.value != null
+    }
+
+    LaunchedEffect(viewModel.error.value) {
+        errorVisibility = viewModel.error.value != null
+    }
+
+    LaunchedEffect(viewModel.loading.value) {
+        loadingVisibility = viewModel.loading.value
     }
 
     ConstraintLayout(
@@ -48,65 +55,59 @@ fun DetailScreen(
             .semantics { contentDescription = "detail" }
             .fillMaxSize()
     ) {
-        val (image, content) = createRefs()
+        val (allViews, error, loading) = createRefs()
 
-        val guideline = createGuidelineFromTop(0.3f)
-
-        SquareView(
+        AnimatedVisibility(
             modifier = Modifier
-                .fillMaxWidth()
-                .constrainAs(image) {
+                .wrapContentSize()
+                .constrainAs(loading) {
                     top.linkTo(parent.top)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
-                }
-                .alpha(0.5f)
+                    bottom.linkTo(parent.bottom)
+                },
+            visible = loadingVisibility
         ) {
-            Image(
-                modifier = Modifier.fillMaxWidth(),
-                painter = rememberImagePainter(
-                    data = viewModel.detail.value?.featuredImage,
-                    builder = {
-                        crossfade(true)
-                        scale(Scale.FILL)
-                    }
-                ),
-                contentDescription = "detail image",
-                contentScale = ContentScale.Crop
+            CircularProgress(
+                Modifier
+                    .width(50.dp)
+                    .height(50.dp)
             )
         }
 
-        Card(
+        AnimatedVisibility(
             modifier = Modifier
-                .fillMaxWidth()
-                .constrainAs(content) {
-                    top.linkTo(guideline)
+                .wrapContentSize()
+                .constrainAs(error) {
+                    top.linkTo(parent.top)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
                     bottom.linkTo(parent.bottom)
-                    height = Dimension.fillToConstraints
                 },
-            shape = RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp)
+            visible = errorVisibility
         ) {
-            AnimatedContent(
-                modifier = Modifier
-                    .wrapContentWidth()
-                    .wrapContentHeight(),
-                targetState = expanded
-            ) { target ->
-                if (target) {
-                    DetailContent(
-                        title = viewModel.detail.value?.title.toString(),
-                        list = viewModel.detail.value?.ingredients ?: listOf()
-                    )
-                } else {
-                    CircularProgress(
-                        Modifier
-                            .width(50.dp)
-                            .height(50.dp)
-                    )
-                }
+            ScreenError(
+                modifier = Modifier.wrapContentSize(),
+                errorMessage = viewModel.error.value
+            ) {
+                viewModel.getRecipeById()
             }
+        }
+
+        AnimatedVisibility(
+            modifier = Modifier
+                .wrapContentSize()
+                .constrainAs(allViews) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom)
+                },
+            visible = contentVisibility
+        ) {
+            DetailContent(
+                viewModel = viewModel
+            )
         }
     }
 }
